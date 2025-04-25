763f2b137389d2266a82ae2a0e202b1505b95415
Tanya Srivastava <tanya.srivastava2310@gmail.com>
2025-04-25 16:16:03 +0530

    message

--- src/repository/UserRepository.java
+++ src/repository/UserRepository.java
@@ -1,23 +1,46 @@
 package repository;
 
-import entity.User;
-import java.util.ArrayList;
-import java.util.List;
+import entity.*;
+
+import java.util.*;
 
 public class UserRepository {
-    private List<User> users = new ArrayList<>();
+    private final Map<String, User> users = new HashMap<>();
+    private final Map<String, Librarian> librarians = new HashMap<>();
 
     public UserRepository() {
-        users.add(new User("librarian", "lib123", "Librarian"));
-        users.add(new User("user", "user123", "User"));
+        users.put("U001", new User("U001", "Tanya"));
+        users.put("U002", new User("U002", "ANimesh"));
+        users.put("U003", new User("U003", "Yashasvi"));
+        users.put("U004", new User("U004", "Riddhima"));
+        librarians.put("L001", new Librarian("L001", "Librarian John"));
     }
 
-    public User login(String username, String password) {
-        for (User user : users) {
-            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                return user;
-            }
-        }
-        return null; 
+    public Map<String, User> getAllUsers() {
+        return users;
+    }
+
+    public Collection<Librarian> getAllLibrarians() {
+        return librarians.values();
+    }
+
+    public User getUserById(String id) {
+        return users.get(id);
+    }
+
+    public Librarian getLibrarianById(String id) {
+        return librarians.get(id);
+    }
+
+    public void addUser(User user) {
+        users.put(user.getId(), user);
+    }
+
+    public boolean deleteUser(String id) {
+        return users.remove(id) != null;
+    }
+
+    public void updateUser(User updatedUser) {
+        users.put(updatedUser.getId(), updatedUser);
     }
 }
